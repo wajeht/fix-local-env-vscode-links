6cf34726bd8b1893fd80c93e96c003eec0505c58
Jaw <jaw.nyein@reclique.com>
2021-12-28 17:01:52 -0500

    c
    c

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
 (() => {
-  try {
-    const links = document.links;
-    for (const l of links) {
-      const href = l.href;
-      const regex = /reclique.core-utils/;
-      const myPath = "vscode://file/Users/jaw/web_dev/core/core/";
-      if (href.match(regex)) {
-        const theRest = href.split("/").slice(4).join("/");
-        l.href = myPath + theRest;
-        l.innerText = myPath + theRest;
+  class Setting {
+    #CORE_DASHBOARD_PAGE = "/reclique.core-utils/";
+    #MEMBERSHIP_PAGE = "/file/";
+    #VSCODE = "vscode://file";
+
+    setCoreGitPath() {
+      let pathInLocalStorage = null;
+      pathInLocalStorage = localStorage.getItem("coreGitPath");
+
+      if (!pathInLocalStorage) {
+        const userPath = prompt("What's your core git folder?");
+        localStorage.setItem("coreGitPath", userPath);
+        pathInLocalStorage = userPath;
+      }
+
+      return pathInLocalStorage;
+    }
+
+    fixPath(userPath) {
+      try {
+        const allLinksInDOMs = document.links;
+        for (const link of allLinksInDOMs) {
+          const currentLink = link.href;
+          if (currentLink.match(this.#CORE_DASHBOARD_PAGE)) {
+            const theRest = currentLink.split("/").slice(4).join("/");
+            link.href = this.#VSCODE + userPath + theRest;
+            link.innerText = this.#VSCODE + userPath + theRest;
+          }
+
+          if (currentLink.match(this.#MEMBERSHIP_PAGE)) {
+            const theRest = currentLink.split("/").slice(7).join("/");
+            link.href = this.#VSCODE + userPath + "/" + theRest;
+            link.innerText = this.#VSCODE + userPath + theRest;
+          }
+        }
+      } catch (err) {
+        alert(err);
       }
     }
-  } catch (err) {
-    alert(err);
   }
-})();
 
+  (function main() {
+    const mySetting = new Setting();
+    const path = mySetting.setCoreGitPath();
+    mySetting.fixPath(path);
+  })();
+})();
